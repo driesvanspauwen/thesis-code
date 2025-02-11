ab0c906f1d1137a16e41c74e6799702caa75f1b3
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-02-11 14:19:10 +0100

    add wm folders

--- emulation/wm-emulation/analysis/disassember.py
+++ emulation/wm-emulation/analysis/disassember.py
@@ -6,5 +6,6 @@ from capstone import *
 from emulator import *
 
 cs = Cs(CS_ARCH_X86, CS_MODE_64)
-for i in cs.disasm(X86_CODE32_LOOP, 0x1000000):
-    print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+code = cs.disasm(X86_CODE32_LOOP, 0x1000000)
+for instr in code:
+    print("0x%x:\t%s\t%s" %(instr.address, instr.mnemonic, instr.op_str))
