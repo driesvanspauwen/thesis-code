dd2b4a28c651f3aa056c5475c59ce1a6bc2b40f5
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2025-03-03 11:29:17 +0000

    remove ghost-is-the-machine code

--- weird_machines/ghost-is-the-machine/Transient-Weird-Machine/run_amd.sh
+++
@@ -1,9 +0,0 @@
-echo "** Testing exception-based gates..."
-cd exceptions
-make clean
-make amd
-./main-amd.elf
-echo ""
-
-echo "** Done!"
-cd ..
--- weird_machines/ghost-is-the-machine/Transient-Weird-Machine/run_arm.sh
+++
@@ -1,9 +0,0 @@
-echo "** Testing ARM gates..."
-echo "** This can take several minutes to complete..."
-cd ARM
-make clean
-make
-./arm.elf
-echo ""
-
-echo "** Done!"
