1340c40d7ca476dc110c877012c2bf3b6be4b050
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2025-03-07 23:05:10 +0000

    code comments

--- src/assign/assign_gate.asm
+++ src/assign/assign_gate.asm
@@ -6,10 +6,15 @@ section .text
 global _start
 
 _start:
-mov [r14], byte 0          ; Caches the input register (sets input of assign gate to 1)
+; Set input
+mov [r14], byte 0          ; Include this line to set logical input to 1 (by caching input register)
+
+; Trigger division by zero exception
 xor rdx, rdx               ; rdx = 0 (clear rdx for division)
-div dl                     ; Divide rax by dl (rdx = 0)
-movzx rcx, byte [r14]      ; rcx = value at address pointed by r14
+div dl                     ; Divide rax by dl (dl is lower 8 bits of rdx)
+
+; Set output (assign)
+movzx rcx, byte [r14]      ; rcx = value at address pointed by r14 (X[0])
 mov rdx, rcx               ; Move rcx to rdx for output address calculation
-add rdx, r15               ; Add the base address of out1 to rdx
-mov dl, byte [rdx]         ; Load the value from memory at the calculated address into dl
+add rdx, r15               ; Add the base address of out1 to rdx (Y[X[0]])
+mov dl, byte [rdx]         ; Cache the value at Y[X[0]] by loading it to dl
